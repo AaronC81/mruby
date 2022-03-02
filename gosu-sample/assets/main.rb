65bc4deda589b82270ac81fd3c29a26634ace217
Aaron Christiansen <aaronc20000@gmail.com>
2022-03-02 02:57:59 +0000

    Use `update` in example

--- gosu-sample/assets/main.rb
+++ gosu-sample/assets/main.rb
@@ -1,11 +1,18 @@
 class Window < Gosu::Window
   def initialize
+    @x = 10
+    @y = 10
     super(500, 500)
   end
 
   def draw
-    Gosu.draw_rect(10, 10, 100, 100, Gosu::Color::RED)
+    Gosu.draw_rect(@x, @y, 100, 100, Gosu::Color::RED)
+  end
+
+  def update
+    puts "Update..."
+    @x += 2
   end
 end
 
-Window.new.tick
+Window.new.show
